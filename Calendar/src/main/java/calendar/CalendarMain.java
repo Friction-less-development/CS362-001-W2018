99b3b18ba3391b263f65243d0919e2eadb76ddee
Ben <catsrnice@hotmail.com>
2018-01-14 23:09:04 -0800

    Fixing build errors

--- Calendar/src/main/java/calendar/CalendarMain.java
+++ Calendar/src/main/java/calendar/CalendarMain.java
@@ -33,6 +33,7 @@ public class CalendarMain {
 		//get todays date
     	Calendar rightnow = Calendar.getInstance();
     	//current month/year/date is today
+    	thisMonth = rightnow.get(Calendar.MONTH)+1;
 		thisYear = rightnow.get(Calendar.YEAR);
 		thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
 
